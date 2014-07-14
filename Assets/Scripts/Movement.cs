405ec25f377e15dd1da839add618de8b7c737084
AamodWalavalkar <Aamod@Ariel.local>
2014-07-13 19:59:56 -0700

    Simple stage scene
    Just some objects and a simple script to play around with the shadows
    for the 3D world.

---
+++ Assets/Scripts/Movement.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Movement : MonoBehaviour {
+
+	public float shift;
+	public float shiftx;
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		shift = transform.position.z - 0.01f;
+		shiftx = transform.position.x + 0.01f;
+		transform.position = new Vector3(shiftx,0.0f,shift);
+	}
+}
+
